a213417bc1da12fe7d0c010c9104aae9fb57bc9c
HyeRyeong <34434153+ryeong904@users.noreply.github.com>
2022-08-01 20:26:47 +0900

    [스택] 프린터

---
+++ [PGS]/level2/프린터.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 0
+
+    queue = deque(priorities)
+    while len(queue) != 0:
+        print(queue)
+        flag = True
+        for i in range(1, len(queue)):
+            if queue[0] < queue[i]:
+                flag = False
+                break
+        if flag == False : # 우선순위가 낮으면 뒤로보냄
+            queue.append(queue[0])
+            queue.popleft()
+            if location == 0 :
+                location += len(queue) -1
+            else:
+                location -= 1
+        else: # 괜찮으면 pop
+            answer += 1
+            queue.popleft()
+            if location == 0:
+                break
+            location -= 1
+    
+    return answer
